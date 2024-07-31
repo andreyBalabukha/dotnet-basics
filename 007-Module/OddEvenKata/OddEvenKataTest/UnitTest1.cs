d15afb4260e359a237d9d4d92263840fa1df718a
andreybalabukha <5344208@gmail.com>
2024-07-31 10:30:35 +0800

    [module-7] second Kata implementation

--- 007-Module/OddEvenKata/OddEvenKataTest/UnitTest1.cs
+++ 007-Module/OddEvenKata/OddEvenKataTest/UnitTest1.cs
@@ -11,14 +11,31 @@ public class Tests
     public void Test1()
     {
         var oddEven = new OddEven();
-        oddEven.generateOddEvenSequence(10);
-        Assert.Pass();
+        Assert.That(oddEven.generateOddEvenSequence(2), Is.EqualTo("1, Even"));
     }
 }
 
 public class OddEven {
 
+    private List<string> oddEvenList = new List<string>();
+
+    private void checkForOddEven(int number) {
+        if (number % 2 == 0) {
+            oddEvenList.Add("Even");
+        } else {
+            oddEvenList.Add(number.ToString());
+        }
+    }
+
     public string generateOddEvenSequence(int number) {
-        return "123";
+        if (number < 1 && number > 100 ) {
+            return "Invalid number";
+        }
+
+        for (int i = 1; i <=number; i++) {
+            checkForOddEven(i);
+        }
+
+        return string.Join(", ", oddEvenList);
     }
 }
