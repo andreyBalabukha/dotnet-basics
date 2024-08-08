d6bbffa3b9f59d263abe6093078be3fb87bd956f
andreybalabukha <5344208@gmail.com>
2024-08-08 15:15:33 +0800

    [008-module] adding logger

--- 009-Module/Logging/Logging/BrainstormSessions/Program.cs
+++ 009-Module/Logging/Logging/BrainstormSessions/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Templates;
+using Serilog.Templates.Themes;
 
 namespace BrainstormSessions
 {
@@ -9,8 +11,34 @@ namespace BrainstormSessions
     {
         public static void Main(string[] args)
         {
-            
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Debug)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Fatal)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Verbose)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(new ExpressionTemplate(
+                    "[{@t:HH:mm:ss} {@l:u3}{#if @tr is not null} ({substring(@tr,0,4)}:{substring(@sp,0,4)}){#end}] {@m}\n{@x}",
+                    theme: TemplateTheme.Code))
+                .CreateLogger();
+            try 
+            {
+                Log.Information("Starting up the host");
+                CreateHostBuilder(args).Build().Run();
+            }
+
+            catch (System.Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+            }
+
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
